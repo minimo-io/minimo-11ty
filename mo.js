3e48da3392ce848c2f091d711c473a87eb7f036d
minimo-io <nicolas@minimo.io>
2023-11-07 14:52:44 -0300

    mobile menu and toggle

--- mo.js
+++ mo.js
@@ -1,1 +1,7 @@
-console.log("OK");
+const primaryNav = document.querySelector(".nav");
+const navToggle = document.querySelector(".nav-toggle");
+
+navToggle.addEventListener('click', () =>{
+    const visibility = primaryNav.getAnimations("data-opened");
+    alert(visibility);
+});
