30aeec3aaa854042cdb04cc97043136e8fe697ec
minimo-io <nicolas@minimo.io>
2023-11-29 21:40:51 -0300

    create components in layouts

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,11 +1,13 @@
 module.exports = function(eleventyConfig){
 
+
     // eleventyConfig.addPassthroughCopy('./src/css/style.css');
     eleventyConfig.addPassthroughCopy('./src/images');
     // eleventyConfig.addPassthroughCopy('./src/js/bundle.js');
     eleventyConfig.addPassthroughCopy({
         './src/js/bundle.js': 'bundle.js'
     });
+    eleventyConfig.addPassthroughCopy({ 'src/robots.txt': '/robots.txt' });
 
     eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
 
