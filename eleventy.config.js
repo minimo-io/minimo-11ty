6b01dfcb2bcb7c157a1ffef54ea78e745ddf37d2
minimo-io <nicolas@minimo.io>
2024-03-05 22:18:19 -0300

    Markdown now is clean from tags!

--- eleventy.config.js
+++ eleventy.config.js
@@ -11,13 +11,24 @@ module.exports = function(eleventyConfig){
     eleventyConfig.addPassthroughCopy('src/.well-known');
 
     eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
+    // if content is markdown then wrap it around, else: free will!
+    // because sometimes I need more flexibility in expense of clarity
+    eleventyConfig.addShortcode("templatey", function(content){
+        let templateExtension = this.page.inputPath.toString().split('.').pop();
+        if (templateExtension == "md"){
+            return `<div class="wrapper ismarkdown">${content}</div>`;
+        }else{
+            return content;
+        }
+    });
+
     eleventyConfig.addFilter("postDate", (dateString) => {
         dateObj = new Date(dateString);
         let monthName = dateObj.toLocaleString('en', { month: 'long' });
         let dayNumber = dateObj.getDate();
         let yearNumber = dateObj.getFullYear();
         return `${monthName} ${dayNumber}, ${yearNumber}`;
-    });    
+    });
 
     // reading time
     eleventyConfig.addPlugin(emojiReadTime, {
