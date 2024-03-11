c48b0d87df795b2eadcc9483deac767f8f5be333
minimo-io <nicolas@minimo.io>
2024-03-11 14:49:06 -0300

    Finished drafts func.

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,6 +1,6 @@
 const pluginEmojiReadTime = require("@11tyrocks/eleventy-plugin-emoji-readtime");
 const pluginSyntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
-
+const pluginDrafts = require("./eleventy.config.drafts.js");
 
 module.exports = function(eleventyConfig){
 
@@ -41,6 +41,10 @@ module.exports = function(eleventyConfig){
     eleventyConfig.addPlugin(pluginSyntaxHighlight, {
         preAttributes: { tabindex: 0 }
     });
+
+    // drafts functionality as in https://www.11ty.dev/docs/quicktips/draft-posts/
+    eleventyConfig.addPlugin(pluginDrafts);
+
 
     return {
         dir: {
